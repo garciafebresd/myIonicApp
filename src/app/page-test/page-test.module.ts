410cd3d06676cbc14d9ac4d5ff61eed0a30365e6
daniel garcia <dgarcia@position.cl>
2018-12-03 14:20:53 -0300

    agregando boton y ruteo del mismo

---
+++ src/app/page-test/page-test.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PageTestPage } from './page-test.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PageTestPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PageTestPage]
+})
+export class PageTestPageModule {}
---
+++ src/app/page-test/page-test.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-test',
+  templateUrl: './page-test.page.html',
+  styleUrls: ['./page-test.page.scss'],
+})
+export class PageTestPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
