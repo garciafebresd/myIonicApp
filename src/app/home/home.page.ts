60be00d40ada8bcc7b9d2e7ba3f763062b39b5c6
daniel garcia <dgarcia@position.cl>
2018-12-18 10:06:50 -0300

    consumiento api-rest service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,43 @@
 import { Component } from '@angular/core';
 
+import { LoadingController } from '@ionic/angular';
+import { RestApiService } from '../rest-api.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+    
+    data1: any;
+    data2: any;
+    data3: any;
+    data4: any;
+    
+    constructor(public api: RestApiService, public loadingController: LoadingController) { }
+    
+    async getData() {
+      const loading = await this.loadingController.create({
+        message: 'Loading'
+      });
+      await loading.present();
+      this.api.getData()
+        .subscribe(res => {
+          console.log(res);
+          this.data1 = res[0];
+          this.data2 = res[1];
+          this.data3 = res[2];
+          this.data4 = res[3];
+          loading.dismiss();
+        }, err => {
+          console.log(err);
+          loading.dismiss();
+        });
+    }
+    
+    ngOnInit() {
+      this.getData();
+    }
+    
+}
